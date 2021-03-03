9eec48ee2d3873647a09c1dcfca38d996a7c1feb
Robbe Bierebeeck <77198548+Robbe-coder@users.noreply.github.com>
2021-03-03 12:50:46 +0100

    on enter the value gets taken from the input feeld and shown in the console

--- lab3-ES6/src/app.js
+++ lab3-ES6/src/app.js
@@ -37,8 +37,8 @@ class Note {
   
       // HINT🤩
       // pressing the enter key in the text field triggers the createNote function
-      // this.txtTodo = ???
-      // this.txtTodo.addEventListener("keypress", this.createNote.bind(this));
+        this.txtTodo = document.querySelector("#taskInput");
+        this.txtTodo.addEventListener("keypress", this.createNote.bind(this));
       // read up on .bind() -> we need to pass the current meaning of this to the eventListener
       // when the app loads, we can show previously saved noted from localstorage
       // this.loadNotesFromStorage();
@@ -50,6 +50,11 @@ class Note {
     }
   
     createNote(e) {
+        if(e.key === "Enter"){
+            e.preventDefault();
+            console.log(this.txtTodo.value);
+        }
+        
       // this function should create a new note by using the Note() class
       // HINT🤩
       // note.add();
@@ -64,5 +69,6 @@ class Note {
   }
   
   let app = new App();
+
   
   
