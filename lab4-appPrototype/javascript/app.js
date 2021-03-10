7155d0fb3f5a0a072d99cda6db869c91403cc624
Robbe Bierebeeck <77198548+Robbe-coder@users.noreply.github.com>
2021-03-10 22:15:09 +0100

    added feature: calling second api with another song

--- lab4-appPrototype/javascript/app.js
+++ lab4-appPrototype/javascript/app.js
@@ -1,6 +1,7 @@
 class App {
     constructor(){
         console.log("start");
+        this.itunesTrack2();
         this.getLocation();
         this.lat; 
         this.long;
@@ -21,16 +22,15 @@ class App {
         else{
             let temperatures = localStorage.getItem("temps");
             temperatures = JSON.parse(temperatures);
-            console.log(temperatures.time);
             let newTimeStamp = Math.round(new Date().getTime()/1000);
-            console.log(newTimeStamp - temperatures.time)
             if(newTimeStamp - temperatures.time > 7200){
                 localStorage.clear();
                 this.getWeather();
             }
+
+            
         }
     }
-
     getWeather(){
         const appId="1c3e89a1826d749a036f49c987727437";
         let url = `http://api.openweathermap.org/data/2.5/weather?lat=${this.lat}&lon=${this.long}&appid=${appId}&units=metric`;
@@ -39,7 +39,6 @@ class App {
             return response.json();
         }).then(data =>{
             console.log(data);
-            //this.itunes(data.main.temp);
             let temperatures = {"description": data.weather[0].description, "temperature": data.main.temp, "time": Math.round(new Date().getTime()/1000)};
             localStorage.setItem('temps', JSON.stringify(temperatures));
         }).then(err =>{
@@ -47,8 +46,8 @@ class App {
         })
     }
 
-    itunes(temp){
-        console.log(temp);
+    itunesTrack1(){
+        
         let url = `https://cors-anywhere.herokuapp.com/https://itunes.apple.com/search?term=nelly`;
         fetch(url).then(response => {
             console.log(response);
@@ -63,5 +62,23 @@ class App {
             console.log(err);
         })
     }
+
+    itunesTrack2(){
+        
+        let url = `https://itunes.apple.com/search?term=vanilla+ice`;
+
+        fetch(url).then(response => {
+            console.log(response);
+            return response.json();
+        }).then(data =>{
+            console.log(data.results[4].artworkUrl100);
+            console.log(data.results[4].collectionViewUrl);
+            console.log(data.results[4].trackName);
+            console.log(data.results[4].artistName);
+        }).then(err =>{
+            console.log(err);
+        })
+    }
+    
 }
 let app = new App();
