eb915691f050cf54dfa9e3a22b1a12e723b9e780
Robbe Bierebeeck <77198548+Robbe-coder@users.noreply.github.com>
2021-03-10 13:06:05 +0100

    added feature: the weather of your current location can be logged into the console

--- lab4-appPrototype/javascript/app.js
+++ lab4-appPrototype/javascript/app.js
@@ -9,7 +9,21 @@ class App {
             let lat = position.coords.latitude;
             let long= position.coords.longitude;
             console.log(position.coords.latitude, position.coords.longitude);
+            this.getWeather(lat, long);
           });
+    }
+    getWeather(lat, long){
+        console.log("ja");
+        const appId="1c3e89a1826d749a036f49c987727437"
+        let url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${appId}`;
+        fetch(url).then(response => {
+            console.log(response);
+            return response.json();
+        }).then(data =>{
+            console.log(data);
+        }).then(err =>{
+            console.log(err);
+        })
     }
 
 }
