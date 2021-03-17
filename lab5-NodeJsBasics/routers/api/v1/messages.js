649a44aca4f3acbf8c17f974584988caeb426c4a
Robbe Bierebeeck <77198548+Robbe-coder@users.noreply.github.com>
2021-03-17 20:13:40 +0100

    moved files: moved functions from routers folder to controllers

--- lab5-NodeJsBasics/routers/api/v1/messages.js
+++ lab5-NodeJsBasics/routers/api/v1/messages.js
@@ -2,40 +2,11 @@ const express = require("express");
 const router = express.Router();
 const messageController = require("../../../controllers/api/v1/messages");
 
-router.get("/",(req, res) => {
-    console.log(req.params);
-    console.log(req.query.user);//it posts your name here
-    if(req.query.user){
-        const username = req.query.user;
-        res.json({
-            "message":`GETTING message for username ${username}`
-        });
-    }else{
+router.get("/", messageController.getAll);
 
-        res.json({
-            "message": "GETTIG messages"
-        });
-    }
-  });
-router.get("/:id", (req,res) => {
-    const id = req.params.id;
-    res.json({
-        "message": `GETTING message with ID ${id}`
-    });
-})
-router.post("/", (req,res) =>{
-    res.json({"message":"POSTING  a new message for user Pikachu"});
-})
-router.put("/:id", (req,res) =>{
-    const id = req.params.id;
-    res.json({
-        "message":`UPDATING a message with id ${id}`
-    });
-})
-router.delete("/:id",(req,res)=>{
-    const id = req.params.id;
-    res.json({
-        "message":`DELETING a message with id ${id}`
-    }); 
-})
+router.get("/:id", messageController.getId);
+
+router.post("/", messageController.post);
+router.put("/:id", messageController.update);
+router.delete("/:id",messageController.remove);
 module.exports= router;
