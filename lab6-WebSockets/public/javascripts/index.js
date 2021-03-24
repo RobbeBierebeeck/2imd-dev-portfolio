25d8c12534591f8168d1adab94541780fc3ebe42
Robbe Bierebeeck <77198548+Robbe-coder@users.noreply.github.com>
2021-03-24 19:33:08 +0100

    feat: added on to views

--- lab6-WebSockets/public/javascripts/index.js
+++ lab6-WebSockets/public/javascripts/index.js
@@ -1,1 +1,10 @@
-console.log("werk aub");
+const submit = document.querySelector(".submit");
+
+submit.addEventListener("click",(e)=>{
+    e.preventDefault();
+    const dropdown = document.querySelector("#teams");
+    const score = document.querySelector("#score");
+    console.log(score.value);
+    console.log(dropdown.value);
+   
+})
