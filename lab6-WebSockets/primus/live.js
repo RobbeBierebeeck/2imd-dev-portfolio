6f9b318f8dc4042db6578c2cbac59ea571d438d6
Robbe Bierebeeck <77198548+Robbe-coder@users.noreply.github.com>
2021-03-24 22:23:00 +0100

    feat: data from form can be broadcasted in node with primus

--- lab6-WebSockets/primus/live.js
+++ lab6-WebSockets/primus/live.js
@@ -4,6 +4,10 @@ function go(server){
    let primus = new Primus(server, {/* options */});
    primus.on('connection', (spark)=> {
     console.log("we have a spark");
+
+    spark.on('data',(data)=>{
+        console.log(data);
+    });
   });
 }
 module.exports.go = go;
--- lab6-WebSockets/public/javascripts/index.js
+++ lab6-WebSockets/public/javascripts/index.js
@@ -10,9 +10,13 @@ const submit = document.querySelector(".submit");
 
 submit.addEventListener("click",(e)=>{
     e.preventDefault();
-    const dropdown = document.querySelector("#teams");
-    const score = document.querySelector("#score");
-    console.log(score.value);
-    console.log(dropdown.value);
+    const dropdown = document.querySelector("#teams").value;
+    const score = document.querySelector("#score").value;
+    console.log(score);
+    console.log(dropdown);
+    primus.write({
+            score: score,
+            team: dropdown
+    });
    
 })
