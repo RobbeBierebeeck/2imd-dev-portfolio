b9cfaaca42b334ec9d067ffc210ccc8852ceacdf
Robbe Bierebeeck <77198548+Robbe-coder@users.noreply.github.com>
2021-03-24 18:48:57 +0100

    feat: added routes and views

--- lab6-WebSockets/routes/index.js
+++ lab6-WebSockets/routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('dashboard/index', {});
 });
-
+router.get('/dashboard', function(req, res, next) {
+  res.render('dashboard/dashboard', {});
+});
 module.exports = router;
