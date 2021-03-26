02013782b3656b85cacfdd6e2c20a4c2bd524cc9
Robbe Bierebeeck <77198548+Robbe-coder@users.noreply.github.com>
2021-03-26 22:44:10 +0100

    refactor: changed router structure

--- lab6-WebSockets/routes/index.js
+++ lab6-WebSockets/routes/index.js
@@ -2,13 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {});
-});
 router.get('/updatestats', function(req, res, next) {
   res.render('dashboard/index', {});
 });
-router.get('/dashboard', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('dashboard/dashboard', {});
 });
 module.exports = router;
