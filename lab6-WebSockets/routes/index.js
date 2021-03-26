f6f1382dc182f2c13e5b6cd05b1e2b257c339621
Robbe Bierebeeck <77198548+Robbe-coder@users.noreply.github.com>
2021-03-26 21:31:22 +0100

    refactor: changed routername

--- lab6-WebSockets/routes/index.js
+++ lab6-WebSockets/routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('index', {});
+});
+router.get('/updatestats', function(req, res, next) {
   res.render('dashboard/index', {});
 });
 router.get('/dashboard', function(req, res, next) {
